620a1835be3876a880646d6fdb4e3e04001f3322
Marlon <marlonffernandes@gmail.com>
2025-01-30 11:06:12 -0300

    alembic

--- fast_zero/tests/test_db.py
+++ fast_zero/tests/test_db.py
@@ -4,11 +4,10 @@ from fast_zero.models import User
 
 
 def test_create_user(session):
-
-    user = User(username="test1", password="test", email="test1@test.com")
+    user = User(username='test1', password='test', email='test1@test.com')
     session.add(user)
     session.commit()
-    result = session.scalar(select(User).where(User.email == "test1@test.com"))
+    result = session.scalar(select(User).where(User.email == 'test1@test.com'))
     session.refresh(user)
 
     assert user.id == 1

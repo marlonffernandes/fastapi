e544498015345916ce06e5df235642d0d83227bf
Marlon <marlonffernandes@gmail.com>
2025-01-31 01:35:17 -0300

    add misc

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,77 +1,86 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI, HTTPException
-from sqlalchemy import create_engine, select
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
+from fast_zero.database import get_session
 from fast_zero.models import User
 from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
-from fast_zero.settings import Settings
 
 app = FastAPI()
 
 fake_database = []
 
 
-@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {"message": "Hello, World!"}
+    return {'message': 'Hello, World!'}
 
 
-@app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema):
-    engine = create_engine(Settings().DATABASE_URL)
-
-    with Session(engine) as session:
-        db_user = session.scalar(
-            select(User).where(
-                (User.username == user.username) | (User.email == user.email)
-            )
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
         )
-        if db_user:
-            if db_user.username == user.username:
-                raise HTTPException(
-                    status_code=HTTPStatus.BAD_REQUEST,
-                    detail="Username already exists",
-                )
-            elif db_user.email == user.email:
-                raise HTTPException(
-                    status_code=HTTPStatus.BAD_REQUEST,
-                    detail="Email already exists",
-                )
+    )
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Username already exists',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.BAD_REQUEST,
+                detail='Email already exists',
+            )
 
-        db_user = User(username=user.username, email=user.email, password=user.password)
-
-        session.add(db_user)
-        session.commit()
-        session.refresh(db_user)
+    db_user = User(
+        username=user.username, email=user.email, password=user.password
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
     return db_user
 
 
-@app.get("/users/", status_code=HTTPStatus.OK, response_model=UserList)
-def read_users():
-    return {"users": fake_database}
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users(session: Session = Depends(get_session)):
+    users = session.scalars(select(User))
+    return {'users': users}
 
 
-@app.get("/users/{user_id}", status_code=HTTPStatus.OK, response_model=UserPublic)
+@app.get(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
 def read_user(user_id: int):
     return fake_database[user_id - 1]
 
 
-@app.put("/users/{user_id}", status_code=HTTPStatus.OK, response_model=UserPublic)
+@app.put(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic
+)
 def update_user(user_id: int, user: UserSchema):
     if user_id < 1 or user_id > len(fake_database) + 1:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found")
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
 
     user_with_id = UserDB(**user.model_dump(), id=user_id)
     fake_database[user_id - 1] = user_with_id
     return user_with_id
 
 
-@app.delete("/users/{user_id}", status_code=HTTPStatus.OK, response_model=Message)
+@app.delete(
+    '/users/{user_id}', status_code=HTTPStatus.OK, response_model=Message
+)
 def delete_user(user_id: int):
     if user_id < 1 or user_id > len(fake_database) + 1:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found")
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
 
     del fake_database[user_id - 1]
-    return {"message": "User deleted"}
+    return {'message': 'User deleted'}

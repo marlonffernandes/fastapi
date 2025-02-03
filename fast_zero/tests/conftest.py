639006c8d8682d71f6ac73576dbb304d361bf71e
Marlon <marlonffernandes@gmail.com>
2025-02-03 18:13:27 -0300

    session implementation

--- fast_zero/tests/conftest.py
+++ fast_zero/tests/conftest.py
@@ -6,7 +6,7 @@ from sqlalchemy.pool import StaticPool
 
 from fast_zero.app import app
 from fast_zero.database import get_session
-from fast_zero.models import table_registry
+from fast_zero.models import User, table_registry
 
 
 @pytest.fixture
@@ -26,10 +26,19 @@ def session():
     engine = create_engine(
         'sqlite:///:memory:',
         connect_args={'check_same_thread': False},
-        poolclass=StaticPool(),
+        poolclass=StaticPool,
     )
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
         yield session
     table_registry.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def user(session):
+    user = User(username='Teste', email='teste@test.com', password='testest')
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -1,7 +1,6 @@
 from http import HTTPStatus
 
-# Tests must run independently
-# Testing fake database
+from fast_zero.schemas import UserPublic
 
 
 def test_read_root_should_return_ok_and_hello_world(client):
@@ -30,28 +29,17 @@ def test_create_user(client):
 def test_read_users(client):
     response = client.get('/users/')  # act
     assert response.status_code == HTTPStatus.OK  # assert
-    assert response.json() == {
-        'users': [
-            {
-                'id': 1,
-                'username': 'testusername',
-                'email': 'testemail@example.com',
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_read_user(client):
-    response = client.get('/users/1')  # act
+def test_read_users_with_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')  # act
     assert response.status_code == HTTPStatus.OK  # assert
-    assert response.json() == {
-        'id': 1,
-        'username': 'testusername',
-        'email': 'testemail@example.com',
-    }
+    assert response.json() == {'users': [user_schema]}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -81,7 +69,7 @@ def test_update_non_existent_user(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('users/1')
     assert response.json() == {'message': 'User deleted'}
 
